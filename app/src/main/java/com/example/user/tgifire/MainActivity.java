79ecb15ab84a1a6cecd61c23853dc96856f6d592
lojpark <lojpark@gmail.com>
2018-05-28 00:14:54 +0900

    Feat: Intent to SignInActivity when click buttonAdmin

--- app/src/main/java/com/example/user/tgifire/MainActivity.java
+++ app/src/main/java/com/example/user/tgifire/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.user.tgifire;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Context mContext = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonAdmin = findViewById(R.id.buttonAdmin);
+        buttonAdmin.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intentToSignInActivity = new Intent(mContext, SignInActivity.class);
+                startActivity(intentToSignInActivity);
+            }
+        });
     }
 }
