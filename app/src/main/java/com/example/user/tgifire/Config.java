7ae4d8dab31af960f6fe8dd24a9782b1e2297a60
lojpark <lojpark@gmail.com>
2018-05-28 22:49:54 +0900

    Add: AuthHelper class and Config class

---
+++ app/src/main/java/com/example/user/tgifire/Config.java
@@ -0,0 +1,10 @@
+package com.example.user.tgifire;
+
+public class Config {
+    static final String CLIENT_ID = "0977dbb2025f4b92803714871513b2d0"; // AKA application ID
+    static final String DEVICE_ID = "ca56bd172eab42aea1e35f6f9335e8da";
+
+    // MUST be consistent with "AUTH REDIRECT URL" of your application set up at the developer.artik.cloud
+    static final String REDIRECT_URI = "cloud.artik.example.hellocloud://oauth2callback";
+
+}
--- app/src/main/java/com/example/user/tgifire/SignInActivity.java
+++ app/src/main/java/com/example/user/tgifire/SignInActivity.java
@@ -3,10 +3,20 @@ package com.example.user.tgifire;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import net.openid.appauth.AuthorizationService;
+
 public class SignInActivity extends AppCompatActivity {
+    AuthorizationService mAuthorizationService;
+    AuthStateDAL mAuthStateDAL;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        mAuthorizationService = new AuthorizationService(this);
+        mAuthStateDAL = new AuthStateDAL(this);
+
+
     }
 }
