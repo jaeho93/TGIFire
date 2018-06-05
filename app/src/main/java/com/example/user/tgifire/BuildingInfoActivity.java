f9a5bce8e4e24bf720c20024c702846e9603fa47
lojpark <lojpark@gmail.com>
2018-06-05 11:36:56 +0900

    Add: loadItemsFromDB method (not implemented yet)

--- app/src/main/java/com/example/user/tgifire/BuildingInfoActivity.java
+++ app/src/main/java/com/example/user/tgifire/BuildingInfoActivity.java
@@ -3,11 +3,45 @@ package com.example.user.tgifire;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class BuildingInfoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_building_info);
+    }
+
+    public boolean loadItemsFromDB(ArrayList<BuildingInfoItem> list) {
+        BuildingInfoItem item ;
+        int i ;
+
+        if (list == null) {
+            list = new ArrayList<BuildingInfoItem>() ;
+        }
+
+        // 순서를 위한 i 값을 1로 초기화.
+        i = 1 ;
+
+        // 아이템 생성.
+        item = new BuildingInfoItem() ;
+        item.setText(Integer.toString(i) + "층") ;
+        list.add(item) ;
+        i++ ;
+        item = new BuildingInfoItem() ;
+        item.setText(Integer.toString(i) + "층") ;
+        list.add(item) ;
+        i++ ;
+        item = new BuildingInfoItem() ;
+        item.setText(Integer.toString(i) + "층") ;
+        list.add(item) ;
+        i++ ;
+        item = new BuildingInfoItem() ;
+        item.setText(Integer.toString(i) + "층") ;
+        list.add(item) ;
+        i++ ;
+
+        return true ;
     }
 }
