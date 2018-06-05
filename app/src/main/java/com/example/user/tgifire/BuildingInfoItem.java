6625a3b010f22803a563ca9af32cf49e3e8b7b6e
lojpark <lojpark@gmail.com>
2018-06-05 11:22:36 +0900

    Add: ListView item class and layout

---
+++ app/src/main/java/com/example/user/tgifire/BuildingInfoItem.java
@@ -0,0 +1,13 @@
+package com.example.user.tgifire;
+
+public class BuildingInfoItem {
+    private String textFloor;
+
+    public void setText(String text) {
+        textFloor = text;
+    }
+
+    public String getText() {
+        return this.textFloor;
+    }
+}
