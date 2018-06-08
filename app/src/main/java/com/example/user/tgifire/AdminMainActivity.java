9638b7f6506013297b8849d375af3dca7e1a8056
lojpark <lojpark@gmail.com>
2018-06-08 13:04:29 +0900

    Fix: Nothing

--- app/src/main/java/com/example/user/tgifire/AdminMainActivity.java
+++ app/src/main/java/com/example/user/tgifire/AdminMainActivity.java
@@ -5,14 +5,9 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -39,7 +34,7 @@ public class AdminMainActivity extends AppCompatActivity {//implements Navigatio
             items[i] = Integer.toString(i + 1) + "층";
         }
 
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, items) ;
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
         listview = (ListView) findViewById(R.id.drawer_menulist);
         listview.setAdapter(adapter) ;
 
